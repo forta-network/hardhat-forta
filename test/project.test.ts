4995827046eea21fa080913680004fff7c35dff9
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-03-09 18:47:19 -0300

    feat: Extend config and add contextPath field

--- test/project.test.ts
+++ test/project.test.ts
@@ -7,40 +7,14 @@ import { ExampleHardhatRuntimeEnvironmentField } from "../src/ExampleHardhatRunt
 import { useEnvironment } from "./helpers";
 
 describe("Integration tests examples", function () {
-  describe("Hardhat Runtime Environment extension", function () {
-    useEnvironment("hardhat-project");
-
-    it("Should add the example field", function () {
-      assert.instanceOf(
-        this.hre.example,
-        ExampleHardhatRuntimeEnvironmentField
-      );
-    });
-
-    it("The example filed should say hello", function () {
-      assert.equal(this.hre.example.sayHello(), "hello");
-    });
-  });
-
   describe("HardhatConfig extension", function () {
     useEnvironment("hardhat-project");
 
-    it("Should add the newPath to the config", function () {
+    it("Should add the contextPath to the config", function () {
       assert.equal(
-        this.hre.config.paths.newPath,
-        path.join(process.cwd(), "asd")
+        this.hre.config.forta.contextPath,
+        path.join(process.cwd(), "agent")
       );
-    });
-  });
-});
-
-describe("Unit tests examples", function () {
-  describe("ExampleHardhatRuntimeEnvironmentField", function () {
-    describe("sayHello", function () {
-      it("Should say hello", function () {
-        const field = new ExampleHardhatRuntimeEnvironmentField();
-        assert.equal(field.sayHello(), "hello");
-      });
     });
   });
 });
