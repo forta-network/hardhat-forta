34ba756f51d763604549792536c6c1b60a3ccbef
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-03-09 19:04:32 -0300

    misc: Remove HRE extension

--- src/type-extensions.ts
+++ src/type-extensions.ts
@@ -2,9 +2,7 @@
 
 // To extend one of Hardhat's types, you need to import the module where it has been defined, and redeclare it.
 import "hardhat/types/config";
-import "hardhat/types/runtime";
 
-import { ExampleHardhatRuntimeEnvironmentField } from "./ExampleHardhatRuntimeEnvironmentField";
 import { FortaUserConfig } from "./types";
 
 declare module "hardhat/types/config" {
@@ -14,13 +12,5 @@ declare module "hardhat/types/config" {
 
   export interface HardhatConfig {
     forta: FortaUserConfig;
-  }
-}
-
-declare module "hardhat/types/runtime" {
-  // This is an example of an extension to the Hardhat Runtime Environment.
-  // This new field will be available in tasks' actions, scripts, and tests.
-  export interface HardhatRuntimeEnvironment {
-    example: ExampleHardhatRuntimeEnvironmentField;
   }
 }
