eded81d75a7e5b053abf97a89372f7606434a46c
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-03-21 14:24:14 -0300

    fix: Remove generated agent config prompts (#9)

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -4,7 +4,6 @@ import fetch from "node-fetch";
 import path from "path";
 import prompts from "prompts";
 import shelljs from "shelljs";
-import format from "string-template";
 
 interface RepositoryTreeNode {
   path: string;
@@ -109,24 +108,6 @@ export async function generateAgent(destinationPath: string) {
     initial: 0,
   });
 
-  const configPrompt = await prompts([
-    {
-      type: "text",
-      name: "protocolName",
-      message: "Protocol name:",
-    },
-    {
-      type: "text",
-      name: "protocolAbbreviation",
-      message: "Protocol abbreviation:",
-    },
-    {
-      type: "text",
-      name: "developerAbbreviation",
-      message: "Developer abbreviation:",
-    },
-  ]);
-
   const files = await getTemplateFiles(templatePrompt.node);
 
   await Promise.all(
@@ -148,45 +129,6 @@ export async function generateAgent(destinationPath: string) {
     )
   );
 
-  const packageJsonPath = path.join(destinationPath, "package.json");
-
-  fs.writeFileSync(
-    packageJsonPath,
-    format(fs.readFileSync(packageJsonPath).toString(), configPrompt)
-  );
-
-  // finds the configuration file regardless of whether the file is an
-  // example config or not, since some templates have an example json
-  // and others don't.
-  const configFilename = shelljs
-    .ls(destinationPath)
-    .find((el) => el.startsWith("agent-config"));
-
-  if (!configFilename) {
-    console.warn(
-      "A configuration file was not found. Skipping config file writing."
-    );
-  } else {
-    const configPath = path.join(destinationPath, configFilename);
-    const configFile = fs.readFileSync(configPath).toString();
-
-    // save the previous config file as an example config
-    fs.writeFileSync(
-      path.join(destinationPath, "agent-config.json.example"),
-      configFile
-    );
-
-    // edit the previous config file and save it as the current config file
-    fs.writeFileSync(
-      path.join(destinationPath, "agent-config.json"),
-      JSON.stringify(
-        Object.assign(JSON.parse(configFile), configPrompt),
-        null,
-        2
-      )
-    );
-  }
-
   console.log("");
   console.log(
     bold(
@@ -197,7 +139,7 @@ export async function generateAgent(destinationPath: string) {
   );
   console.log(
     bold(
-      "Don't forget to finish setting up agent-config.js with your desired behavior!"
+      "Configuration instructions are described in the SETUP.md file inside the agent folder."
     )
   );
 }
